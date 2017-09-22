c56b72cd23349ee1be095449257d79368d9ac0b8
Godzoco <godzoco@qq.com>
2017-09-23 01:07:12 +0800

    是

---
+++ Python编程快速上手3.6/7/7183test.py
@@ -0,0 +1,18 @@
+#!python3
+# Chap7PracStrip
+# Remove whitespace from beginning and end of string using regex
+
+import re
+
+def remspace(string):
+    global stringrem
+    print('1原来的\n'+string)
+    bspaceRegex = re.compile(r'\s*$')
+    fspaceRegex = re.compile(r'^\s*')
+    stringrem = bspaceRegex.sub('', string)
+    print('2第一个调整去除后面的空\n'+stringrem) 
+    stringrem = fspaceRegex.sub('', stringrem)
+    return stringrem
+
+remspace('    here is the string    ')
+print(stringrem)
