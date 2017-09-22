66e943b4f762f49692032ed1b276966d1dce0731
Godzoco <godzoco@qq.com>
2017-09-23 02:00:20 +0800

    注释

--- Python编程快速上手3.6/7/7183test.py
+++ Python编程快速上手3.6/7/7183test.py
@@ -10,8 +10,10 @@ def remspace(string):
     bspaceRegex = re.compile(r'\s*$')
     fspaceRegex = re.compile(r'^\s*')
     stringrem = bspaceRegex.sub('', string)
-    print('2第一个调整去除后面的空\n3这个就是用没有字符把空字符串代替了\n'
-          '4也就是先结尾用空的‘’代替，再头用‘’代替\n'+stringrem) 
+    print('2第一个调整去除后面的空\n'+
+          '3这个就是用没有字符把空字符串代替了\n'
+          '4也就是先结尾用空的‘’代替，再头用‘’代替\n'
+          '5先看后面，用$结束，用‘’直接替换了结束末尾的空格\n'+stringrem) 
     stringrem = fspaceRegex.sub('', stringrem)
     return stringrem
 
