e17efa85396867c824b2625cad28628d72dc3465
Godzoco <godzoco@qq.com>
2017-09-23 01:35:18 +0800

    3

--- Python编程快速上手3.6/7/75.py
+++ Python编程快速上手3.6/7/75.py
@@ -7,7 +7,7 @@ a=p.findall('dssssssssssssddd415-555-9999   212-555-0000')
 print(a)
 
 p1=re.compile(r'((\d\d\d)-(\d\d\d)-(\d\d\d\d))')
-a1=p1.findall('dssssssssssssddd    415-555-9999   212-555-0000')
+a1=p1.findall('dssssssssssssddd1    415-555-9999   212-555-0000')
 print(a1)
 
 p2=re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
