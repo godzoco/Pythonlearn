b4e789189a4d87b62e6688e0db7bb9352a80e40f
Godzoco <godzoco@qq.com>
2017-12-14 20:04:20 +0800

    #打开浏览器

---
+++ Python编程快速上手3.6/11/ping192.168.1.1.py
@@ -0,0 +1,4 @@
+#webbrowser模块的open() 可以打开新的浏览器 打开指定的url
+
+import os
+os.system("ipconfig")
---
+++ Python编程快速上手3.6/11/打开路由器.py
@@ -0,0 +1,4 @@
+#webbrowser模块的open() 可以打开新的浏览器 打开指定的url
+
+import webbrowser
+webbrowser.open('http://192.168.1.1')
