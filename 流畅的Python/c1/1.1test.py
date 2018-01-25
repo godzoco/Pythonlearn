a39ff92356f3caf214da5b1728726ef44a5f4348
Godzoco <godzoco@qq.com>
2018-01-25 15:02:22 +0800

    流畅的Python

---
+++ 流畅的Python/c1/1.1test.py
@@ -0,0 +1,30 @@
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+#collections
+print([str(n) for n in range(2, 11)])
+print(list('JQKA'))
+print('spades diamonds clubs hearts'.split())
+
+print(len(FrenchDeck()))
+deck = FrenchDeck()
+print(deck[0])
+print(deck[1])
+print(deck[:3])
+print(deck[12::13])
